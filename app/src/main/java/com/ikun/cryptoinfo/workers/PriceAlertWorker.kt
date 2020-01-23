7569b4b2e0fc924bd73071de4a700111eaa80c5d
Kunal <ikunal03@gmail.com>
2020-01-23 01:39:59 -0500

    Intent Firing Done

--- app/src/main/java/com/ikun/cryptoinfo/workers/PriceAlertWorker.kt
+++ app/src/main/java/com/ikun/cryptoinfo/workers/PriceAlertWorker.kt
@@ -1,10 +1,15 @@
 package com.ikun.cryptoinfo.workers
 
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.ikun.cryptoinfo.activities.MainActivity
 import com.ikun.cryptoinfo.data.PriceAlertData
 import com.ikun.cryptoinfo.interfaces.TransactionInterface
 import retrofit2.Call
@@ -27,6 +32,8 @@ class PriceAlertWorker(
                 response: Response<PriceAlertData>
             ) {
                 if (response.body()?.last?.toFloat()!! > thresholdPrice) {
+
+
                     val noti = NotificationCompat.Builder(applicationContext, "1")
                         .setSmallIcon(android.R.drawable.sym_def_app_icon)
                         .setContentTitle("Currency Prices Changed !")
@@ -35,6 +42,19 @@ class PriceAlertWorker(
                         .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                         .setAutoCancel(true).build()
+
+                    val resultIntent = Intent(applicationContext, MainActivity::class.java)
+                    val resultPendingIntent: PendingIntent?
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        resultPendingIntent =
+                            TaskStackBuilder.create(applicationContext).run {
+                                addNextIntentWithParentStack(resultIntent)
+                                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+                            }
+                        noti.apply { contentIntent = resultPendingIntent }
+                    }
+
+
                     with(NotificationManagerCompat.from(applicationContext)) {
                         notify(1, noti)
                     }
